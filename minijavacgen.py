3c672d280bef2f90ad608d987fc0f8467b2d9bb4
moutella <joaogabrielmsc@gmail.com>
2019-12-04 23:15:37 -0300

    altos cgens

--- minijavacgen.py
+++ minijavacgen.py
@@ -1,5 +1,6 @@
 from minijavaparse import result
 
+
 print(result)
 def uniqueprint(entrada):
     if type(entrada) == tuple:
@@ -10,7 +11,11 @@ def uniqueprint(entrada):
 
 def cgen(entrada):
     if type(entrada) == tuple:
-        if entrada[0] == "mexp":
+        if entrada[0] == "rexp":
+            cgenrexp(entrada)
+        elif entrada[0] == "aexp":
+            cgenaexp(entrada)
+        elif entrada[0] == "mexp":
             cgenmexp(entrada)
         elif entrada[0] == "sexp":
             cgensexp(entrada)
@@ -19,10 +24,73 @@ def cgen(entrada):
                 cgen(item)
     else:
         if type(entrada) == str:
-            pass
+            cgenstring(entrada)
         else:
             cgenint(entrada)
-
+def cgenexp(entrada):
+    if len(entrada) == 4:
+        cgen(entrada[1])
+        print("sw $a0 0($sp)")
+        print("addiu $sp $sp -4")
+        cgen(entrada[3])
+        print("lw $t1 4($sp)")
+        current_branch = branchcounter
+        label = "branch_{}".format(current_branch)
+        label_end = "branch_end{}".format(current_branch)
+        print("beq $a0 $zero {}".format(label))
+        print("beq $t1 $zero {}".format(label))
+        print("li $a0 1")
+        print("j {}".format(label_end))
+        print("{}:".format(label))
+        print("li $a0 0")
+        print("{}:".format(label_end))
+        print("addiu $sp $sp 4")
+        add_branch_counter()
+    else:
+        cgen(entrada[1])
+def cgenrexp(entrada):
+    if len(entrada) == 4:
+        cgen(entrada[1])
+        print("sw $a0 0($sp)")
+        print("addiu $sp $sp -4")
+        cgen(entrada[3])
+        print("lw $t1 4($sp)")
+        if entrada[2] == "<":
+            print("slt $a0 $t1 $a0")
+        elif entrada[2] == "==":
+            current_branch = branchcounter
+            label = "branch_{}".format(current_branch)
+            label_end = "branch_end_{}".format(current_branch)
+            print("beq $a0 $t1 {}".format(label))
+            print("li $a0 0")
+            print("j {}".format(labelend))
+            print("{}:".format(labeltrue))
+            print("li $a0 1")
+            add_branch_counter()
+        else:
+            current_branch = branchcounter
+            label = "branch_{}".format(current_branch)
+            label_end = "branch_end_{}".format(current_branch)
+            print("beq $a0 $t1 {}".format(label))
+            print("li $a0 1")
+            print("j {}".format(labelend))
+            print("{}:".format(labeltrue))
+            print("li $a0 0")
+            add_branch_counter()
+        print("addiu $sp $sp 4")
+    else:
+        cgen(entrada[1])
+def cgenaexp(entrada):
+    if len(entrada) == 4:
+        cgen(entrada[1])
+        print("sw $a0 0($sp)")
+        print("addiu $sp $sp -4")
+        cgen(entrada[3])
+        print("lw $t1 4($sp)")
+        print("{} $a0 $t1 $a0".format("add" if entrada[2] == "+" else "sub"))
+        print("addiu $sp $sp 4")
+    else:
+        cgen(entrada[1])
 
 def cgenmexp(entrada):
     if len(entrada) == 4:
@@ -37,9 +105,37 @@ def cgenmexp(entrada):
         cgen(entrada[1])
 
 def cgensexp(entrada):
-    cgen(entrada[1])
+    if len(entrada) == 3:
+        cgen(entrada[2])
+        if entrada[1] == '!':
+            current_branch = branchcounter
+            label = "branch_{}".format(current_branch)
+            labelend = "branch_end_{}".format(current_branch)
+            print("beq $a0 $zero {}".format(label))
+            add_branch_counter()
+            print("li $a0 0")
+            print("j {}".format(labelend))
+            #if branch true
+            print("{}: ".format(label))
+            print("li $a0 1")
+            print("{}: ".format(labelend))
+        else:
+            print("sub $a0 $zero $a0")
+    else:
+        cgen(entrada[1])
+
+def add_branch_counter():
+    global branchcounter
+    branchcounter += 1
 
 def cgenint(entrada):
     print("li $a0 {}".format(entrada))
 
+def cgenstring(entrada):
+    if entrada == "true":
+        print("li $a0 1")
+    elif entrada == "false":
+        print("li $a0 0")
+
+branchcounter = 0
 cgen(result) 
